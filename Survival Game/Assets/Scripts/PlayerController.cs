d88aa63cb45b8e6e9d4a8c03185ae456d6b67e2a
DESKTOP-B7PET4G\Straw <strawdanblood@gmail.com>
2017-03-24 03:03:02 -0400

    These are the project files so far

---
+++ Survival Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float rotateSpeed;
+
+
+    private CharacterController cc;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+ 
+
+
+        // Rotate around y - axis
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+        
+
+    }
+}
